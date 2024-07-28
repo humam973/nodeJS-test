649aa4cdfc55170911be9dd6221c6dbf408258a3
HUMAM SHIHAB ALDEN <humam@HUMAMs-MacBook-Pro.local>
2024-07-28 19:11:48 +0300

    ---Requests & Responses--- 1-request object 2-response object 3-response html page 4-basic routing 5-status code 6-redirects

--- 5 clients & servers/server.js
+++ 5 clients & servers/server.js
@@ -1,8 +1,42 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req , res) => {
-    console.log('request made')
-    
+    console.log(req.url , req.method)
+
+    res.setHeader('contype-Type', 'text/plain')
+
+    let path = './view/'
+
+    switch(req.url){
+        case '/':
+        path += 'index.html' ;
+        res.statusCode = 200 ;
+        break ;
+        case '/about': 
+        path += 'about.html' ;
+        res.statusCode = 200 ;
+        break ;
+        case '/about-me': 
+        res.statusCode = 301 ;
+        res.setHeader('Location' , '/about')
+        res.end() ;
+        break ;
+        default : 
+        path += '404.html' ;
+        res.statusCode = 404;
+        break ;
+    }
+
+    fs.readFile(path, (err , data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        } {
+            res.write(data)
+            res.end()
+        }
+    })
 })
 
 server.listen(3000 , 'localhost' , () => {
